7f0425c9a1805b3be9848aa303dfe737f981e694
Hasnain Abas <ha42479992@gmail.com>
2025-11-19 08:58:21 -0800

    Add files via upload

---
+++ python-oop-inheritance-example.py
@@ -0,0 +1,36 @@
+class Car:    
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    pass
+
+
+# Create object OUTSIDE the class
+ec1 = ElectricCar('Tesla', 'Electric', 2005)
+
+# Increase mileage
+ec1.increment_odometer(20)
+
+# Print updated odometer reading
+ec1.read_odometer()
